32bea26fb02842fcef78d0efb8238a2c52fc696d
SuryaPrakash4477 <71883857+SuryaPrakash4477@users.noreply.github.com>
2024-02-07 17:58:10 +0530

    add image, description and link

--- portfolio.py
+++ portfolio.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(layout="centered")
+st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/SuryaImage.jpg")
@@ -16,3 +17,22 @@ I am Working in Holmium Technology Comoany.
     st.info(content)
 
 st.write("Below you can find some of the apps I ahve Built in Pytho. Feel free to contact me")
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data (1).csv", sep = ";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code]({row['url']})")
+        
+
