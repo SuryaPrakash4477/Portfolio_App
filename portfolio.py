88a14c4762fe3901e9e6ca7afebfd1eac134562e
SuryaPrakash4477 <71883857+SuryaPrakash4477@users.noreply.github.com>
2024-02-07 17:07:17 +0530

    create columns, image and description

---
+++ portfolio.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/SuryaImage.jpg")
+
+with col2:
+    st.title("Surya Prakash Sharma")
+    content = """
+    Hi, I am Surya Prakash Sharma! I am a Python Programmer, Software Engineer-Embedded Team. I graduated in 2023 from Shoolini Unversity, Solan with 8.67 CGPA.
+I am Working in Holmium Technology Comoany.
+    """
+    st.info(content)
